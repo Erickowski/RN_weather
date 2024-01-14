f9ae48e253415ec02f768ff05d226894d03b6799
Erick Daniel Perez Mata <erickdanielperezmata@outlook.es>
2024-01-14 17:47:28 -0600

    Create button animation

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -1,9 +1,43 @@
-import { Text, TextInput, TouchableWithoutFeedback, View } from "react-native";
+import { useMemo } from "react";
 import { Picker } from "@react-native-picker/picker";
+import {
+  Animated,
+  Text,
+  TextInput,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
 
 import { COUNTRIES } from "@types";
 
 export function Form() {
+  const buttonAnimation = useMemo(() => {
+    return new Animated.Value(1);
+  }, []);
+
+  const handleInAnimation = () => {
+    Animated.spring(buttonAnimation, {
+      toValue: 0.8,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  const handleOutAnimation = () => {
+    Animated.spring(buttonAnimation, {
+      toValue: 1,
+      friction: 4,
+      tension: 30,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  const styleAnimation = useMemo(
+    () => ({
+      transform: [{ scale: buttonAnimation }],
+    }),
+    [buttonAnimation]
+  );
+
   return (
     <View>
       <View>
@@ -25,12 +59,18 @@ export function Form() {
           ))}
         </Picker>
       </View>
-      <TouchableWithoutFeedback>
-        <View className="mt-12 bg-black p-3 justify-center">
+      <TouchableWithoutFeedback
+        onPressIn={handleInAnimation}
+        onPressOut={handleOutAnimation}
+      >
+        <Animated.View
+          style={styleAnimation}
+          className="mt-12 bg-black p-3 justify-center"
+        >
           <Text className="text-white font-bold uppercase text-center text-lg">
             Buscar clima
           </Text>
-        </View>
+        </Animated.View>
       </TouchableWithoutFeedback>
     </View>
   );
